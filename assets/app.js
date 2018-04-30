2412b2116a856eb1b406946ec233d814df718e62
Stephen Lanszweert <stephenlanszweert@hotmail.be>
2018-05-01 01:18:42 +0200

    Added the ambulance

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,18 @@
-console.log("Hello app!!");
+function init() {
+  moveAmbulance();
+}
+
+function moveAmbulance() {
+  let ambulance = document.getElementById('ambulance');
+  let initial = 0;
+  setInterval(function() {
+    initial += 25;
+    var width = window.innerWidth - 80;
+    if (initial >= width) {
+      initial = 0;
+    }
+    ambulance.style.left = initial + 'px';
+  }, 500);
+}
+
+init();
